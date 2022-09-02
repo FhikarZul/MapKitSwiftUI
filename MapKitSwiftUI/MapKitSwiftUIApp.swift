904a6a0cd825d7c55eef600d8930c4658f2699e0
Fhikar Zul <Fhikarzul18@gmail.com>
2022-09-02 10:32:33 +0800

    Initial Commit

---
+++ MapKitSwiftUI/MapKitSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitSwiftUIApp.swift
+//  MapKitSwiftUI
+//
+//  Created by Lucy on 02/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MapKitSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
